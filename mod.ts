5faa1a69d4f3b7157fd2144a7c05638656939c54
Aditya Singh <samant04aditya@gmail.com>
2020-06-22 00:19:06 +0530

    added: Whitelists

---
+++ mod.ts
@@ -0,0 +1,70 @@
+import api from "./api.ts";
+import { Application, log, send } from "./deps.ts";
+
+const app = new Application();
+
+const PORT = 8080;
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("INFO"),
+  },
+  loggers: {
+    default: {
+      level: "INFO",
+      handlers: ["console"],
+    },
+  },
+});
+
+app.addEventListener("error", (event) => {
+  log.error(event.error);
+});
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.response.body = "Internal server error";
+    throw err;
+  }
+});
+
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  log.info(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const end = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${end}ms`);
+});
+
+app.use(api.routes());
+app.use(api.allowedMethods());
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png",
+    "/videos/space.mp4",
+  ];
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
+});
+
+if (import.meta.main) {
+  log.info(`Server running on port ${PORT}...`);
+  await app.listen({
+    port: PORT,
+  });
+}
