5c749a09df97ab544f0f05a32fa8eda81c1ff38b
Aditya Singh <samant04aditya@gmail.com>
2020-06-22 00:17:56 +0530

    oak:middleware

---
+++ api.ts
@@ -0,0 +1,52 @@
+import { Router } from "./deps.ts";
+import * as launches from "./models/launches.ts";
+import * as planets from "./models/planets.ts";
+
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = `
+    {___     {__      {_         {__ __        {_       
+    {_ {__   {__     {_ __     {__    {__     {_ __     
+    {__ {__  {__    {_  {__     {__          {_  {__    
+    {__  {__ {__   {__   {__      {__       {__   {__   
+    {__   {_ {__  {______ {__        {__   {______ {__  
+    {__    {_ __ {__       {__ {__    {__ {__       {__ 
+    {__      {__{__         {__  {__ __  {__         {__
+                    Mission Control API`;
+});
+
+router.get("/planets", (ctx) => {
+  ctx.response.body = planets.getAllPlanets();
+});
+
+router.get("/launches", (ctx) => {
+  ctx.response.body = launches.getAll();
+});
+
+router.get("/launches/:id", (ctx) => {
+  if (ctx.params?.id) {
+    const launchesList = launches.getOne(Number(ctx.params.id));
+    if (launchesList) {
+      ctx.response.body = launchesList;
+    } else {
+      ctx.throw(400, "Launch with that id doesn't exists");
+    }
+  }
+});
+
+router.post("/launches", async (ctx) => {
+  const body = await ctx.request.body();
+  launches.addOne(body.value);
+  ctx.response.body = { success: true };
+  ctx.response.status = 201;
+});
+
+router.delete("/launches/:id", async (ctx) => {
+  if (ctx.params?.id) {
+    const result = launches.removeOne(Number(ctx.params.id));
+    ctx.response.body = { success: result };
+  }
+});
+
+export default router;
