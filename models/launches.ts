2c7e3298183c1eb90a0f955e347b624d90df45b5
Aditya Singh <samant04aditya@gmail.com>
2020-06-22 00:20:25 +0530

    launchData

---
+++ models/launches.ts
@@ -0,0 +1,79 @@
+import { flatMap, log } from "../deps.ts";
+
+interface Launch {
+  flightNumber: number;
+  mission: string;
+  rocket: string;
+  customers: Array<string>;
+  launchDate: number;
+  upcoming: boolean;
+  success?: boolean;
+  target?: string;
+}
+
+const launches = new Map<number, Launch>();
+
+async function downloadLaunchData() {
+  log.info("Downloading launch data...");
+  const response = await fetch("https://api.spacexdata.com/v3/launches");
+
+  if (!response.ok) {
+    log.warning("Failed to fetch SpaceX launches");
+    throw new Error("Launch data download failed.");
+  }
+
+  const launchData = await response.json();
+
+  for (const launch of launchData) {
+    const payloads = launch["rocket"]["second_stage"]["payloads"];
+
+    const customers = flatMap(payloads, (payload: any) => {
+      return payload["customers"];
+    });
+
+    const flightData = {
+      flightNumber: launch["flight_number"],
+      mission: launch["mission_name"],
+      rocket: launch["rocket"]["rocket_name"],
+      launchDate: launch["launch_date_unix"],
+      upcoming: launch["upcoming"],
+      success: launch["launch_success"],
+      customers: customers,
+    };
+
+    launches.set(flightData.flightNumber, flightData);
+  }
+}
+
+await downloadLaunchData();
+log.info(`Downloaded data for ${launches.size} SpaceX launches.`);
+
+export function getAll() {
+  return Array.from(launches.values());
+}
+
+export function getOne(id: number) {
+  if (launches.has(id)) {
+    return launches.get(id);
+  }
+  return null;
+}
+
+export function removeOne(id: number) {
+  const aborted = launches.get(id);
+  if (aborted) {
+    aborted.upcoming = false;
+    aborted.success = false;
+  }
+  return aborted;
+}
+
+export function addOne(data: Launch) {
+  launches.set(
+    data.flightNumber,
+    Object.assign(data, {
+      upcoming: true,
+      customers: ["Zero to Mastery", "NASA"],
+    }),
+  );
+}
