f846463f06bee5d052283dc3e0610b730d01549a
Aditya Singh <samant04aditya@gmail.com>
2020-06-22 00:20:07 +0530

    tests: assertEquals

---
+++ models/planets.test.ts
@@ -0,0 +1,57 @@
+/**
+ * Deno includes:
+ * 
+ * 1. The test runner in the CLI.
+ * 2. Assertions in the standard library.
+ * 3. Built-in test fixtures with Deno.test().
+ */
+
+import { assertEquals } from "../test_deps.ts";
+import { filterHabitablePlanets } from "./planets.ts";
+
+const HABITABLE_PLANET = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1",
+  koi_smass: "1",
+};
+
+const NOT_CONFIRMED = {
+  koi_disposition: "FALSE POSITIVE",
+};
+
+const TOO_LARGE_PLANETARY_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1.5",
+  koi_srad: "1",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1.01",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_MASS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1.01",
+  koi_smass: "1.04",
+};
+
+Deno.test("filter only habitable planets", () => {
+  const filtered = filterHabitablePlanets(
+    [
+      HABITABLE_PLANET,
+      NOT_CONFIRMED,
+      TOO_LARGE_PLANETARY_RADIUS,
+      TOO_LARGE_SOLAR_RADIUS,
+      TOO_LARGE_SOLAR_MASS,
+    ],
+  );
+  assertEquals(filtered, [
+    HABITABLE_PLANET,
+  ]);
+});
